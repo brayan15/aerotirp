e8d1caa7e28a7aff74177016da3db567accae310
Bryan Lara Vargas <bralavar@gmail.com>
2021-01-25 19:37:18 -0600

    Initial configuration

---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
+
+describe('App tests', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  it('renders learn react link', () => {
+    const container = wrapper.find('.app')
+
+    expect(container.exists()).toBe(true)
+  })
+})
---
+++ src/setupTests.js
@@ -0,0 +1,18 @@
+
+import Enzyme from 'enzyme'
+import { shallow, render, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+// import 'jest-enzyme'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+/* Object.defineProperty(window, 'matchMedia', {
+  value: jest.fn(() => ({ matches: true }))
+})
+ */
+expect.extend({})
+
+// Make Enzyme functions available in all test files without importing
+global.shallow = shallow
+global.render = render
+global.mount = mount
